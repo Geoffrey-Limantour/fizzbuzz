973b4571f71b2856040741bc381e1d5e52c4c711
Noskeal <noskeal@gmail.com>
2020-06-18 16:04:53 +0200

    modification du test afin de tester bang également avec la nouvelle structure de fonction pour Fizzbuzzcreator

--- test_Fizzbuzzcreator.py
+++ test_Fizzbuzzcreator.py
@@ -1,8 +1,27 @@
 import unittest
-from Fizzbuzzcreator import Fizzbuzzer
+from Fizzbuzzcreator import Fizz
+from Fizzbuzzcreator import Buzz
+from Fizzbuzzcreator import Bang
+from Fizzbuzzcreator import Fizzbuzzbang
+
 
 class FizzbuzzTestCase(unittest.TestCase):
     
+    def test_fizz(self):
+        self.assertEqual(Fizz(3),"Fizz")
+    def test_buzz(self):
+        self.assertEqual(Buzz(5),"Buzz")
+    def test_bang(self):
+        self.assertEqual(Bang(7),"Bang")
+    def test_fizzbuzz(self):
+        self.assertEqual(Fizzbuzzbang(15),"FizzBuzz")
+    def test_fizzbuzzbang(self):
+        self.assertEqual(Fizzbuzzbang(105),"FizzBuzzBang")
+
+"""
+     def test_(self):
+        self.assertEqual(Fizzbuzzer(35),"Bang")
+
     def test_fizz(self):
         self.assertEqual(Fizzbuzzer(3),"Fizz", "pas fizz")
 
@@ -14,3 +33,4 @@ class FizzbuzzTestCase(unittest.TestCase):
 
     def test_normal(self):
         self.assertEqual(Fizzbuzzer(1),1)
+"""
