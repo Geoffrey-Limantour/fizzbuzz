bae3152317f5294ad2c715b7aa479f96c4434b79
Noskeal <noskeal@gmail.com>
2020-06-18 16:04:00 +0200

    modification de la structure de code afin de le rendre plus modulabe

--- Fizzbuzzcreator.py
+++ Fizzbuzzcreator.py
@@ -1,9 +1,42 @@
-def Fizzbuzzer(i): #défini la fonction fizzbuzzer()
-    if i%3 == 0 and i%5 == 0:
-        return("FizzBuzz")
-    elif i%5 == 0:
-        return("Buzz")
-    elif i%3 == 0:
+#def Fizzbuzzer(i): #défini la fonction fizzbuzzer()
+#    if i%3 == 0 and i%5 == 0:
+#        return("FizzBuzz")
+#    elif i%5 == 0:
+#        return("Buzz")
+#    elif i%3 == 0:
+#        return("Fizz")
+#    else:
+#        return(i)
+
+
+def Fizz(i):
+    if i%3 == 0:
+
         return("Fizz")
     else:
         return(i)
+
+def Buzz(i):
+    if i%5 == 0:
+        return("Buzz")
+    else:
+        return(i)
+
+def Bang(i):
+    if i%7 == 0:
+        return("Bang")
+    else:
+        return(i)
+
+def Fizzbuzzbang(i):
+    concat = ""
+    if Fizz(i) == "Fizz" or Buzz(i) == "Buzz" or Bang(i) =="Bang":
+        if Fizz(i) == "Fizz":
+            concat = Fizz(i)
+        if Buzz(i) == "Buzz":
+            concat = concat + "Buzz"
+        if Bang(i) == "Bang":
+            concat = concat + "Bang"
+        return(concat)
+    else:
+        return (i)
